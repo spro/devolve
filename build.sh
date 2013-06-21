8be187788e82152862b16e71ca7c17741f3fd499
Sean Robertson <sprobertson@gmail.com>
2013-06-21 10:37:13 -0700

    restructure and add package.json to seem more like a real module

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+coffee -o bin -c src
+cp bin/devolve.js bin/devolve.js.tmp
+echo "#!/usr/bin/env node" > bin/devolve.js.tmp
+cat bin/devolve.js >> bin/devolve.js.tmp
+mv bin/devolve.js.tmp bin/devolve.js
